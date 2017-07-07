21930261570377e64619f41195bda9cabf944782
ZenBrayn <rwbenz@gmail.com>
2017-07-07 20:22:46 +0900

    Switched back to removing the list columns since they don't work with filtering.

--- server.R
+++ server.R
@@ -11,6 +11,10 @@ library(dplyr)
 shinyServer(function(input, output) {
   # Get the starwars data set ready for display
   tbl_dat <- dplyr::starwars
+  # The last several columns are list columns which
+  # don't currently work with filtering; so
+  # they are excluded
+  tbl_dat <- tbl_dat %>% select(name:species)
   
   output$starwars_tbl <- DT::renderDataTable({
     DT::datatable(tbl_dat, filter = "top")
